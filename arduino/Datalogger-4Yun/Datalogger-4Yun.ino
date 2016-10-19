cc142a38d68b6c10d1b15928f5a75020f74a4cc5
vap0rtranz <vap0rtranz@gmail.com>
2016-10-19 00:27:26 -0500

    adding SD card to sensor reading

---
+++ arduino/Datalogger-4Yun/Datalogger-4Yun.ino
@@ -0,0 +1,84 @@
+/*
+  SD card datalogger
+
+ This example shows how to log data from three analog sensors
+ to an SD card using the SD library.
+
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+
+ created  24 Nov 2010
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+
+#include <SPI.h>
+#include <SD.h>
+// Include remote libs
+#include <Console.h>
+#include <Bridge.h>
+#include <BridgeClient.h>
+
+// Sparkfun SD shield: pin 8
+const int chipSelect = 8;
+
+void setup() {
+   Bridge.begin(); // start bridge b/w Atmega/AVR + AR/Linux
+  // Open serial communications and wait for port to open:
+  Console.begin();
+  while (!Console) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Console.println("\nYou're connected.  Bridge should be up...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Console.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Console.println("SD card initialized.");
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 2; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 1) {
+      dataString += ",";
+    }
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Console.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Console.println("error opening datalog.txt");
+  }
+}
+
+
+
+
+
--- arduino/drrobot-allsensors.ino/drrobot-allsensors.ino.ino
+++ arduino/drrobot-allsensors.ino/drrobot-allsensors.ino.ino
@@ -9,20 +9,25 @@
 #include <Bridge.h>
 #include <Console.h>
 #include <BridgeClient.h>
+#include <SPI.h>
+#include <SD.h>
 
 // configurable global parameters
 const int tempPin = 0;
 const int lightPin = 1;
 const int ledPin = 13; // the pin for Console notification
+const int chipSelect = 8; // Sparkfun SD shield: pin 8
 int samplePeriod = 5000; //1min samples
 float tempCel = 0;
 float lightRelativeLUX = 0;
 int incomingByte;
+File dataFile;
+String CSVMetricsLine = "";
 
 BridgeClient client; //  to communicate using the Yun's bridge & Linux OS.
 
 void setup() {
- // for debugging: Bridge setup may take a few seconds so indicate when it's bridged via PIN 13
+  // for debugging: Bridge setup may take a few seconds so indicate when it's bridged via PIN 13
   pinMode(ledPin,OUTPUT);
   digitalWrite(ledPin, HIGH);
   Bridge.begin(); // start bridge b/w Atmega/AVR + AR/Linux
@@ -30,18 +35,29 @@ void setup() {
   while (!Console){ // enable this if you don't want to start main loop() until your Console connects
     ; // wait for Console port to connect.
   }
+  delay(1000); // give your remote console a second to setup
   Console.println(F("You're connected to my console, and Bridge begun."));
   digitalWrite(ledPin, LOW);
+  
+  // initialize SD Card incase remote publish of data fails
+  if (!SD.begin(chipSelect)) { // check that SD card works
+    Console.println("Card failed, or not present");
+    return;   // don't do anything more:
+  }
+  Console.println("SD card initialized.");
 }
 
 void loop() {
   
   // simple Console check to see if responsive.  IDE has bug for Console over Wifi so always use SSH and/or telnet to verify
-  if (Console.available() > 0) { // see if there's incoming Console data:
+  if (Console.available() > 0) // see if there's incoming Console data:
+  { 
     incomingByte = Console.read(); // read the oldest byte in the serial buffer:
     if (incomingByte == 'H') { digitalWrite(ledPin, HIGH); } // if it's a capital H (ASCII 72), turn on the LED 
     if (incomingByte == 'L') { digitalWrite(ledPin, LOW); } // if it's an L (ASCII 76) turn off the LED
   }
+
+  dataFile = SD.open("data.csv", FILE_WRITE); // open file. only one file can be open at a time,
   
   //take the readings
   tempCel = analogRead(tempPin);
@@ -50,13 +66,14 @@ void loop() {
   // do some calculations
   tempCel = (5*tempCel*100/1024); // the LM35 gets 5V input, linear sensitivity of 1C=10mV, and 10bit sample of Atmega , or 1024 stepping
   
-  // output the metrics
-  Console.print("Temp:"); //Display the temperature on Console monitor
-  Console.print(tempCel);
-  Console.print("C, ");
-  Console.print("Light:");
-  Console.print(lightRelativeLUX);
-  Console.println(",EOL");//print the value to serial   
+  CSVMetricsLine = String(String(tempCel) + "," + String(lightRelativeLUX)); // prep metrics sensor data to file by converting to strings
+  if (dataFile) // did the open filehandle succeed?
+  {
+    dataFile.println(CSVMetricsLine);
+    dataFile.close();
+    Console.println(CSVMetricsLine); 
+  } 
+    else { Console.println("error opening data file!"); }
     
   delay(samplePeriod); //the base sampling wait
 }
